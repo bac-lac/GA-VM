83662f138344a90e55ebde0e442f2e1112af10d4
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2025-12-18 13:59:53 -0500

    Update password data

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "app" {
   key_name                    = "keypair"
   vpc_security_group_ids      = [data.aws_security_group.app.id]
   subnet_id                   = element(data.aws_subnets.app.ids, count.index)
-  #get_password_data           = true
+  get_password_data           = false
   user_data                   = local.windows_user_data
   monitoring                  = true
   ebs_optimized               = true
