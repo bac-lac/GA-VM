984b2df40c3f67b5ad5286d5e9569d4934af1295
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2025-12-22 15:51:35 -0500

    Fix CKV_AWS_161

--- terraform/db.tf
+++ terraform/db.tf
@@ -4,26 +4,27 @@ resource "aws_db_subnet_group" "data" {
 }
 
 resource "aws_db_instance" "ga_mysql" {
-  allocated_storage               = 20
-  apply_immediately               = true
-  auto_minor_version_upgrade      = true
-  backup_retention_period         = 35
-  copy_tags_to_snapshot           = true
-  db_name                         = "ga${var.ENV}"
-  deletion_protection             = true
-  db_subnet_group_name            = aws_db_subnet_group.data.name
-  enabled_cloudwatch_logs_exports = ["audit", "general", "error", "slowquery"]
-  engine                          = "mysql"
-  engine_version                  = "8.0"
-  identifier                      = "ga-db-${var.ENV}"
-  instance_class                  = var.DB_INSTANCE_CLASS
-  monitoring_interval             = 5
-  monitoring_role_arn             = aws_iam_role.ga_rds_monitoring_role.arn
-  parameter_group_name            = "default.mysql8.0"
-  password                        = var.ADMIN_DB_PASSWORD
-  performance_insights_enabled    = true
-  skip_final_snapshot             = true
-  storage_encrypted               = true
-  username                        = var.ADMIN_DB_USERNAME
-  vpc_security_group_ids          = [data.aws_security_group.data.id]
+  allocated_storage                   = 20
+  apply_immediately                   = true
+  auto_minor_version_upgrade          = true
+  backup_retention_period             = 35
+  copy_tags_to_snapshot               = true
+  db_name                             = "ga${var.ENV}"
+  deletion_protection                 = true
+  db_subnet_group_name                = aws_db_subnet_group.data.name
+  enabled_cloudwatch_logs_exports     = ["audit", "general", "error", "slowquery"]
+  engine                              = "mysql"
+  engine_version                      = "8.0"
+  iam_database_authentication_enabled = true
+  identifier                          = "ga-db-${var.ENV}"
+  instance_class                      = var.DB_INSTANCE_CLASS
+  monitoring_interval                 = 5
+  monitoring_role_arn                 = aws_iam_role.ga_rds_monitoring_role.arn
+  parameter_group_name                = "default.mysql8.0"
+  password                            = var.ADMIN_DB_PASSWORD
+  performance_insights_enabled        = true
+  skip_final_snapshot                 = true
+  storage_encrypted                   = true
+  username                            = var.ADMIN_DB_USERNAME
+  vpc_security_group_ids              = [data.aws_security_group.data.id]
 }
