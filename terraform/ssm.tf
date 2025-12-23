b700fb23d1fd1eb171ab80dc3537d975a6bb0cf2
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2025-12-23 13:56:47 -0500

    Add ssm association

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -4,3 +4,19 @@ resource "aws_ssm_parameter" "dir_admin_password" {
   value       = var.DIRECTORY_ADMIN_PASSWORD
   description = "Directory Admin password"
 }
+
+
+resource "aws_ssm_association" "join_domain" {
+  name        = "AWS-JoinDirectoryServiceDomain"
+  targets {
+    key    = "InstanceIds"
+    values = [aws_instance.app[0].id, aws_instance.app[1].id]
+  }
+
+  parameters = {
+    directoryId       = aws_directory_service_directory.directory.id
+    directoryName     = aws_directory_service_directory.directory.name
+    dnsIpAddresses    = join(",", aws_directory_service_directory.v.dns_ip_addresses)
+    directoryUserName = "Admin"
+    directoryPassword = var.DIRECTORY_ADMIN_PASSWORD
+   }
