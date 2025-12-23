5da7ca235d702fdbf7c162e27d58b2e0cd543ef3
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2025-12-23 14:13:59 -0500

    Remove username

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -17,6 +17,5 @@ resource "aws_ssm_association" "join_domain" {
     directoryId       = aws_directory_service_directory.directory.id
     directoryName     = aws_directory_service_directory.directory.name
     dnsIpAddresses    = join(",", aws_directory_service_directory.directory.dns_ip_addresses)
-    directoryUserName = "Admin"
    }
 }
