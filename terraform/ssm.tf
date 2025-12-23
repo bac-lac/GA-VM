81cac14863638e2d3ec14b64f29fea02c2cf813a
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2025-12-23 14:00:48 -0500

    Fix typo

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -16,7 +16,7 @@ resource "aws_ssm_association" "join_domain" {
   parameters = {
     directoryId       = aws_directory_service_directory.directory.id
     directoryName     = aws_directory_service_directory.directory.name
-    dnsIpAddresses    = join(",", aws_directory_service_directory.v.dns_ip_addresses)
+    dnsIpAddresses    = join(",", aws_directory_service_directory.directory.dns_ip_addresses)
     directoryUserName = "Admin"
     directoryPassword = var.DIRECTORY_ADMIN_PASSWORD
    }
