28056298f1263098a53c5baa100aae0cbd30849f
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2025-12-23 13:14:53 -0500

    Fix vpc id

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -15,6 +15,6 @@ resource "aws_vpc_dhcp_options" "ds" {
 }
 
 resource "aws_vpc_dhcp_options_association" "assoc" {
-  vpc_id          = var.vpc_id
+  vpc_id          = data.aws_vpc.vpc.id
   dhcp_options_id = aws_vpc_dhcp_options.ds.id
 }
