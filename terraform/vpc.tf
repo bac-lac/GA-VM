6b067550f58284fe31e1082be7ac773955bb8522
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2025-12-23 13:11:46 -0500

    Add dhcp options

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,3 +4,17 @@ data "aws_vpc" "vpc" {
     values = ["*"]
   }
 }
+
+resource "aws_vpc_dhcp_options" "ds" {
+  domain_name          = aws_directory_service_directory.directory.name
+  domain_name_servers  = aws_directory_service_directory.directory.dns_ip_addresses
+
+  tags = {
+    Name = "gonanywhere-dhcp-options"
+  }
+}
+
+resource "aws_vpc_dhcp_options_association" "assoc" {
+  vpc_id          = var.vpc_id
+  dhcp_options_id = aws_vpc_dhcp_options.ds.ids
+}
