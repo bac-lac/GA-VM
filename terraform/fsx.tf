eeea6d22bf143bfcc7b696d1649cf5525b0260fd
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2025-12-29 10:47:46 -0500

    Fix array

--- terraform/fsx.tf
+++ terraform/fsx.tf
@@ -2,7 +2,7 @@ resource "aws_fsx_windows_file_system" "ga_fsx" {
   active_directory_id           = aws_directory_service_directory.directory.id
   security_group_ids            = [data.aws_security_group.app.id]
   storage_capacity              = 32
-  subnet_ids                    = data.aws_subnets.app.ids[0]
+  subnet_ids                    = [data.aws_subnets.app.ids[0]]
   throughput_capacity           = 32
   weekly_maintenance_start_time = "6:05:00"
 }
