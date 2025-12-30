5e52c9034453a472f8718b15b643d76c3ae85fb6
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2025-12-30 14:28:45 -0500

    Update list

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -16,6 +16,6 @@ resource "aws_ssm_association" "join_domain" {
   parameters = {
     directoryId       = aws_directory_service_directory.directory.id
     directoryName     = aws_directory_service_directory.directory.name
-    dnsIpAddresses    = one(aws_directory_service_directory.directory.dns_ip_addresses)
+    dnsIpAddresses    = tolist(aws_directory_service_directory.directory.dns_ip_addresses)[0]
    }
 }
