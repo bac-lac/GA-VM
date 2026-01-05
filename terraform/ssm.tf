6a3a4a88a00dd09cf61bf2dddba183adb72b773a
champj <jonathan.champagne@bac-lac.gc.ca>
2026-01-05 15:31:33 -0500

    typo

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -28,7 +28,7 @@ resource "aws_ssm_patch_baseline" "patch_baseline" {
 
     patch_filter {
       key    = "MSRC_SEVERITY"
-      values = ["Critical", "Important"]
+      values = ["Important", "Critical"]
     }
     
     patch_filter {
