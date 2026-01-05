bc6e0dac187dc366dd41ebdeb3323c0b525e5317
champj <jonathan.champagne@bac-lac.gc.ca>
2026-01-05 15:09:37 -0500

    S3 security improvments

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -13,3 +13,23 @@ resource "aws_s3_bucket_versioning" "ssm_versioning" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "ssm_public_access_block" {
+  bucket = aws_s3_bucket.ssm_s3.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.ssm_s3.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+    bucket_key_enabled = true
+  }
+}
