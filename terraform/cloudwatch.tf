0144095fef62ef028384115ea84cd90018cd30e4
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2026-01-13 07:59:34 -0500

    Update dimensions

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -99,6 +99,7 @@ resource "aws_cloudwatch_metric_alarm" "ga_cw_ec2_hdd_alarm" {
   statistic                 = "Minimum"
   dimensions = {
     InstanceId              = aws_instance.app[count.index].id
+    instance                = "C:"
   }
   period                    = 60
   evaluation_periods        = 5
