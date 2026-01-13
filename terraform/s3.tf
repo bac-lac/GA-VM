a30550b4e8638633b3a25d1ac7bef9c240622348
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2026-01-13 09:41:06 -0500

    Remove s3 lifecycle

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,14 +1,9 @@
 resource "aws_s3_bucket" "ssm_s3" {
   bucket = "ssm-goanywhere-${var.ENV}"
-     
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "aws_s3_bucket_versioning" "ssm_versioning" {
   bucket = aws_s3_bucket.ssm_s3.id
-
   versioning_configuration {
     status = "Enabled"
   }
@@ -16,7 +11,6 @@ resource "aws_s3_bucket_versioning" "ssm_versioning" {
 
 resource "aws_s3_bucket_public_access_block" "ssm_public_access_block" {
   bucket = aws_s3_bucket.ssm_s3.id
-
   block_public_acls       = true
   ignore_public_acls      = true
   block_public_policy     = true
@@ -25,7 +19,6 @@ resource "aws_s3_bucket_public_access_block" "ssm_public_access_block" {
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.ssm_s3.id
-
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
