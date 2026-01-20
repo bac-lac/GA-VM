e08ca308d5dad740cca49cc12af5991b6676019c
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2026-01-20 14:56:32 -0500

    Add random id to secret

--- terraform/secretsmanager.tf
+++ terraform/secretsmanager.tf
@@ -1,5 +1,9 @@
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
 resource "aws_secretsmanager_secret" "ssh_private" {
-  name       = "ec2/key-pair"
+  name       = "ec2/key-pair-${var.env}-${random_id.suffix.hex}"
 }
 
 resource "aws_secretsmanager_secret_version" "ssh_private_version" {
