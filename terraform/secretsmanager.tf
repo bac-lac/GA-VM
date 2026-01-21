ef54a0088c9af27344d6941adc37c4e163791bea
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2026-01-21 07:18:16 -0500

    Fix typo

--- terraform/secretsmanager.tf
+++ terraform/secretsmanager.tf
@@ -3,7 +3,7 @@ resource "random_id" "suffix" {
 }
 
 resource "aws_secretsmanager_secret" "ssh_private" {
-  name       = "ec2/key-pair-${var.env}-${random_id.suffix.hex}"
+  name       = "ec2/key-pair-${var.ENV}-${random_id.suffix.hex}"
 }
 
 resource "aws_secretsmanager_secret_version" "ssh_private_version" {
