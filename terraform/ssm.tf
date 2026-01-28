7614b9878b70c400717511aac5baf655e8c9cf7c
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2026-01-28 15:33:28 -0500

    Rollback to single quote

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -188,7 +188,7 @@ resource "aws_ssm_association" "cwagent_start_windows_ssm_param" {
   # This is the Run Command parameter schema for AWS-RunPowerShellScript
   parameters = {
     commands = [
-      "& \"C:\\Program Files\\Amazon\\AmazonCloudWatchAgent\\amazon-cloudwatch-agent-ctl.ps1\" -a fetch-config -m ec2 -c ssm:/GoAnywhere-$${var.ENV}/ec2/amazon-cloudwatch-agent/config/windows -s"
+      "& 'C:\\Program Files\\Amazon\\AmazonCloudWatchAgent\\amazon-cloudwatch-agent-ctl.ps1' -a fetch-config -m ec2 -c ssm:/GoAnywhere-$${var.ENV}/ec2/amazon-cloudwatch-agent/config/windows -s"
     ]
   }
 
