3cf5af944577dd41b177156dd1a74e6f20567ba6
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2026-03-02 09:30:16 -0500

    Remove fsx backup

--- terraform/fsx.tf
+++ terraform/fsx.tf
@@ -1,11 +1,12 @@
 resource "aws_fsx_windows_file_system" "ga_fsx" {
-  active_directory_id           = aws_directory_service_directory.directory.id
-  security_group_ids            = [data.aws_security_group.app.id]
-  storage_capacity              = var.FSX_STORAGE_CAPACITY
-  subnet_ids                    = [data.aws_subnets.app.ids[0]]
-  throughput_capacity           = var.FSX_THROUGHPUT_CAPACITY
-  weekly_maintenance_start_time = "6:05:00"
+  active_directory_id             = aws_directory_service_directory.directory.id
+  automatic_backup_retention_days = 0
+  security_group_ids              = [data.aws_security_group.app.id]
+  storage_capacity                = var.FSX_STORAGE_CAPACITY
+  subnet_ids                      = [data.aws_subnets.app.ids[0]]
+  throughput_capacity             = var.FSX_THROUGHPUT_CAPACITY
+  weekly_maintenance_start_time   = "6:05:00"
   tags = {
-      Name                      = "GoAnywhere-${var.ENV}"
+      Name                        = "GoAnywhere-${var.ENV}"
     }
 }
