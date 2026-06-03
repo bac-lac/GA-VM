76cbf0dcb6e999aeb81cb49c90a3785c84356312
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2026-06-03 12:43:47 -0400

    Update ssl policy

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -6,7 +6,7 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn   = data.aws_lb.ga_alb.arn
   port                = "443"
   protocol            = "HTTPS"
-  ssl_policy          = "ELBSecurityPolicy-FS-1-2-Res-2019-08"
+  ssl_policy          = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06"
   certificate_arn     = aws_acm_certificate.baclacgcca.arn
   default_action {
     type = "fixed-response"
